96efbf4a0350eb19e9213a9d63a9fb6f6a1f6fc2
m.kodama <m.kodama@dreamcareer.co.jp>
2022-04-19 19:03:08 +0900

    練習問題１～４実装：済

--- routes/api/index.js
+++ routes/api/index.js
@@ -15,4 +15,22 @@ router.get("/tasks", async function (req, res, next) {
   res.send(getTasks);
 });
 
+/* タスクを１件取得するルーティング */
+router.get("/tasks/:id", async function (req, res, next) {
+  const getTasksId = await tasks.getTasksId(req.params.id);
+  res.send(getTasksId);
+});
+
+/* タスクを１件更新するルーティング */
+router.patch("/tasks/:id", async function (req, res, next) {
+  const patchTasksId = await tasks.patchTasksId(req.params.id, req.body);
+  res.send(patchTasksId);
+});
+
+/* タスク一覧を削除するルーティング */
+router.delete("/tasks/:id", async function (req, res, next) {
+  const deleteTasksId = await tasks.deleteTasksId(req.params.id);
+  res.send(deleteTasksId);
+});
+
 module.exports = router;
--- src/tasks.js
+++ src/tasks.js
@@ -49,5 +49,82 @@ getTasks = async function () {
   }
 };
 
+/**
+ *  タスクを１件取得するAPI
+ *
+ *  @returns レスポンス　JSON
+ */
+getTasksId = async function (id) {
+  let connection = null;
+  try {
+    connection = await mysql.createConnection(config.dbSetting);
+    // ここにSQLを記述する
+    const sql =
+      "SELECT t_task.id, t_task.category_id, m_category.category_name, t_task.task_name, t_task.deadline, t_task.task_status, t_task.updated_at, t_task.created_at FROM t_task LEFT JOIN m_category ON t_task.category_id = m_category.id WHERE t_task.id = ?";
+    let d = [id];
+    const [rows, fields] = await connection.query(sql, d);
+    return rows;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    connection.end();
+  }
+};
+
+/**
+ *  タスクを１件更新するAPI
+ *
+ *  @returns レスポンス　JSON
+ */
+patchTasksId = async function (id, body) {
+  let connection = null;
+  try {
+    connection = await mysql.createConnection(config.dbSetting);
+    // ここにSQLを記述する
+    const sql =
+      "UPDATE t_task SET task_name=?, deadline=?, category_id=?, task_status=?, updated_at=? WHERE id=?;";
+    let d = [
+      body.taskName,
+      body.deadline,
+      body.category,
+      body.status,
+      new Date(),
+      id,
+    ];
+    const [rows, fields] = await connection.query(sql, d);
+    return rows;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    connection.end();
+  }
+};
+
+/**
+ *  タスクを１件削除するAPI
+ *
+ *  @returns レスポンス　JSON
+ */
+deleteTasksId = async function (id) {
+  let connection = null;
+  try {
+    connection = await mysql.createConnection(config.dbSetting);
+    // ここにSQLを記述する
+    const sql = "DELETE from t_task WHERE id = ?;";
+    let d = [id];
+    const [rows, fields] = await connection.query(sql, d);
+
+    // console.log(rows);
+    return rows;
+  } catch (err) {
+    console.log(err);
+  } finally {
+    connection.end();
+  }
+};
+
 exports.postTasks = postTasks;
 exports.getTasks = getTasks;
+exports.getTasksId = getTasksId;
+exports.patchTasksId = patchTasksId;
+exports.deleteTasksId = deleteTasksId;
